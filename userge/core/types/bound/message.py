d14e4b4dd3866a1e759cfa9a6d18a1f0f5eb12ff
Kakashi <ashwinstr@gmail.com>
2022-05-04 21:37:50 +0530

    testing

--- userge/core/types/bound/message.py
+++ userge/core/types/bound/message.py
@@ -42,7 +42,7 @@ class Message(RawMessage):
         self._process_canceled = False
         self._module = module
         self._kwargs = kwargs
-        self._client = None
+        self._client = client
         super().__init__(client=client, **mvars)
 
     @classmethod
